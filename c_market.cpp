c11e66687d59b99e1ccb45e7074796906463e938
Robert <rob@mail.l>
2015-05-18 09:37:30 +0200

    market: delete copy

--- c_market.cpp
+++ c_market.cpp
@@ -1,11 +1,11 @@
 #include "c_market.h"
 
-void c_market::buy(e_currency currency, unsigned int nuber_of)
+void c_market::buy(e_currency currency, unsigned int nuber_of, unsigned int ID)
 {
 
 }
 
-void c_market::sell(e_currency currency, unsigned int nuber_of)
+void c_market::sell(e_currency currency, unsigned int nuber_of, unsigned int ID)
 {
 
 }
--- c_market.h
+++ c_market.h
@@ -9,8 +9,11 @@ enum class e_currency {
 class c_market
 {
 public:
-  void buy(e_currency currency, unsigned int nuber_of);
-  void sell(e_currency currency, unsigned int nuber_of);
+  c_market() = delete;
+  c_market(const c_market & mark) = delete;
+  c_market & operator = (const c_market &) = delete;
+  void buy(e_currency currency, unsigned int nuber_of, unsigned int ID);
+  void sell(e_currency currency, unsigned int nuber_of, unsigned int ID);
 };
 
 #endif // C_MARKET_H
